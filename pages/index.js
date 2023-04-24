7cd1725d84d224f40902f563083d4a339f3fdd2e
Your Name <you@example.com>
2023-04-24 16:33:26 -0400

    commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { fetchAllPosts } from "../src/api/api";
+import Article from "../src/components/Article/Article";
+import Form from "../src/components/Form/Form";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ posts }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Movies</title>
+        <meta name="description" content="Find the best web dev content" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Form />
+      <div className={styles.articles}>
+        {posts.map((post) => {
+          return <Article key={post.id} post={post} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const posts = await fetchAllPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h5>movie reviews</h5>
+    </footer>
+  );
+};
+
+export default Footer;
